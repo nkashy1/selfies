8e99836636c2fa878ebaf904432b80a3877d95ca
Neeraj Kashyap <nkashy1@gmail.com>
2016-06-04 02:54:21 -0700

    Moving on up

---
+++ deconstruct.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys
+
+script_file = sys.argv[1]
+args = sys.argv[2:]
+
+with open(script_file, 'r') as script_body:
+    script = script_body.read()
+
+    if len(args) == 0:
+        print(script)
+    else:
+        instructions = script.format(*args)
+        print(instructions)
